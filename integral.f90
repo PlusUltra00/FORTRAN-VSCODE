ce0cf9225b9eab0369a30a814cf589d6f20c17e8
GoldenRecord <89250932+PlusUltra00@users.noreply.github.com>
2022-11-20 00:54:50 +0900

    edit integral.f90

--- integral.f90
+++ integral.f90
@@ -1,30 +1,27 @@
 program main
     implicit none
-    INTEGER n
-    double precision, PARAMETER:: a = -1.0d0
-    double precision, PARAMETER:: b = 1.0d0
+    integer n
+    double precision, parameter:: a = -1.0d0
+    double precision, parameter:: b = 1.0d0
     double precision s_daikei
+
     n = 10
 
     call daikei(n, a, b, s_daikei)
 
-    WRITE(*, *) s_daikei
-
+    write(*, *) s_daikei
 end program main
 
 subroutine daikei(n, a, b, s_daikei)
     implicit none
-    INTEGER, intent(in) :: n
+    integer, intent(in) :: n
     double precision, intent(in) :: a, b
-    double precision, INTENT(OUT):: s_daikei
-    double precision h
-    double precision f
-    INTEGER i
-    double precision f1, f2
+    double precision, intent(out):: s_daikei
+    double precision h, f, f1, f2
+    integer i
 
-    h = (b - a) / DBLE(n)
-    s_daikei = 0.0d0
-
+    h = (b - a) / dble(n)
+    s_daikei = 0
     do i = 0, n - 1
         f1 = f(a + i * h)
         f2 = f(a + (i + 1.0d0) * h)
@@ -34,8 +31,7 @@ end subroutine daikei
 
 function f(xin)
     implicit none
-    double precision, INTENT(IN):: xin
+    double precision, intent(in) :: xin
     double precision f
-
     f = 2 / (1 + xin ** 2)
 end function f
